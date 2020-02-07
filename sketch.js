d54452066650c7ed4f7d3ba4870c774d79ba3655
MarcusElle <46597619+Marcus4420@users.noreply.github.com>
2020-02-07 09:48:41 +0100

    Label frem for dyret selv

--- sketch.js
+++ sketch.js
@@ -98,15 +98,15 @@
       txtbox = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     }
     if (CurrAni == "Kat") {
-      txtbox = "JEG ER EN Kat"
+      txtbox = "JEG ER EN: " + label
     }
     if (CurrAni == "Papegøje") {
-      txtbox = "JEG ER EN Papegøje"
+      txtbox = "JEG ER EN:" + label
     }
     if (CurrAni == "Marsvin") {
-      txtbox = "JEG ER EN Marsvin"
+      txtbox = "JEG ER EN: " + label
     }
     if (CurrAni == "Kanin") {
-      txtbox = "JEG ER EN Kanin"
+      txtbox = "JEG ER EN: " + label
     }
   }
