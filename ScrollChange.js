41ace6bb35fbc2a5551ab897de0b114a51f03b75
BuskenT <OskarTLH@users.noreply.github.com>
2020-02-07 12:10:19 +0100

    Endelig fikset

--- ScrollChange.js
+++ ScrollChange.js
@@ -11,7 +11,7 @@
             var y = window.scrollY;
             var ul = document.getElementById('menubar');
             console.log(y*100/360/2);
-            ul.style.backgroundColor = 'hsla('+y*100/360/2+', 100%, 50%, 0.5)';
+            ul.style.backgroundColor = 'hsla('+y*100/360/2+', 100%, 50%, 1)';
             if (y<=0) {
                   ul.style.backgroundColor = 'white';
             }
